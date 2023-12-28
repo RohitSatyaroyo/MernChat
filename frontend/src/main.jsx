ac7ec0d0c72c7cdc8715447ed5c4647588870c94
RohitSatyaroyo <122684136+RohitSatyaroyo@users.noreply.github.com>
2023-12-28 14:39:29 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
     <App />
-  </React.StrictMode>,
+ 
 )
